ec495131e5b8ea590298450fa1ace9725fe943fc
Binod <binod@Binitas-MacBook-Air.local>
2018-12-17 21:40:41 -0700

    take it easy- done

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log("It's raining today!!!");
+
+//1. Data Types
+// A light switch that can be either on or off.
+    //Boolean => because true can be used for on and false for off.
+// A user's email address.
+    //A String => becasue it contain combination of letters and words.
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+    //An object => because well they are objects and we can store their properties.
+// A list of student names from our class.
+    //An array => because these are without any properties.
+// A list of student names from our class, each with a location.
+    //object => becasue location can be stored as properties.
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+    //An array with objects nested inside.
+
+//2. Take it easy
+//1.
+ 
+const rainbowColors = ["red", "Orange", "yellow", "green", "blue", "indigo", "violet"];
+
+console.log(rainbowColors[4]);
+
+const binod = {
+    favoriteFood: "pineapple",
+    hobby: "soccer",
+    city: "Denver",
+    favoriteDatatype: "object"
+}
+
+console.log(binod.hobby);
