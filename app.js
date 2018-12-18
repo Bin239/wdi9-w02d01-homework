4ff8b54dc4bb27aa59da959a60b549cc2a2f62c1
Binod <binod@Binitas-MacBook-Air.local>
2018-12-17 22:07:11 -0700

    crazyObject

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ console.log("It's raining today!!!");
     //An array with objects nested inside.
 
 //2. Take it easy
-//1.
  
 const rainbowColors = ["red", "Orange", "yellow", "green", "blue", "indigo", "violet"];
 
@@ -29,3 +28,49 @@ const binod = {
 }
 
 console.log(binod.hobby);
+
+//3. crazy Object
+
+const crazyObject = {
+    taco: [
+      {
+        meat: 'steak',
+        cheese: ['panela', 'queso', 'chihuahua']
+      },
+      {
+        meat: 'chicken',
+        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+      },
+    ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [
+        {
+          name: "Jeff",
+          occupation: "manager"
+        },
+        {
+          name: "funkhauser",
+          occupation: "tv dude"
+        },
+        {
+          name: "susie",
+          occupation: "jeffs wife",
+          favourtieHobby: "Swearing at Larry and Jeff"
+        },
+      ]
+    }
+  }
+
+  console.log(crazyObject.taco[1].salsa[5]);
+
+  console.log(crazyObject.larry.quotes[0]);
+
+  console.log(crazyObject.larry.characters[2].favourtieHobby);
+
+  console.log(crazyObject.larry.nicknames[1]);
+
+  console.log(crazyObject.larry.characters[1])
+
+
